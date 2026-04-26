12fc3116d075f3e7a70356edd95226c03ad58ea4
rafaelbaumgarten-collab <rafael.baumgarten@escola.pr.gov.br>
2026-04-26 16:48:25 -0300

    Delete meu-primeiro-script.lua

--- meu-primeiro-script.lua
+++
@@ -1,8 +0,0 @@
--- Meu Primeiro Script do GitHub
-print("✅ Script carregado com sucesso do GitHub!")
-
-game.Players.PlayerAdded:Connect(function(player)
-    print("👋 " .. player.Name .. " entrou no servidor!")
-end)
-
-print("Script feito por " .. game.Players.LocalPlayer.Name)
